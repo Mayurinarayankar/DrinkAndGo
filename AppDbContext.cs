918bc2f4834f316515f2c7165532ed0c402c0a49
Mayurinarayankar <mayurinarayankar901@gmail.com>
2025-11-25 09:26:59 +0530

    feat: Added AppDbContext for Drink And Go

---
+++ AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DrinkAndGoMVC.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Identity.Client;
+
+namespace DrinkAndGoMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set;}
+    }
+}
