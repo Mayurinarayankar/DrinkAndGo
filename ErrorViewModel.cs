a76a9b8a5a4744ecce1ca48c3fa1b5a23b53b77c
Mayurinarayankar <mayurinarayankar901@gmail.com>
2025-11-25 09:34:35 +0530

    feat: added the Error model view

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DrinkAndGoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
