c9637353a4b57088ac9a86fceb21d5219be25986
Mayurinarayankar <mayurinarayankar901@gmail.com>
2025-11-25 09:32:16 +0530

    feat: Added the Mock repository

---
+++ MockCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using DrinkAndGoMVC.Data.Interfaces;
+using DrinkAndGoMVC.Data.Models;
+
+namespace DrinkAndGoMVC.Data.Mocks
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories { 
+            get
+                
+            {
+                return new List<Category>
+                {
+                    new Category {CategoryName= "Alcholic", Description="All Alcholic Drinks"},
+                    new Category{CategoryName="Non Alcholic", Description="Non Alcholic Drinks"}
+                };
+            }
+            set => throw new NotImplementedException();
+        }
+    }
+}
