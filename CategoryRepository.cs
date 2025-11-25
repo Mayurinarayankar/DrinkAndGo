679579875d859cccb459fb210849fc390e3d617a
Mayurinarayankar <mayurinarayankar901@gmail.com>
2025-11-25 09:29:34 +0530

    feat: Added the Category and Drink

---
+++ CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using DrinkAndGoMVC.Data.Interfaces;
+using DrinkAndGoMVC.Data.Models;
+
+namespace DrinkAndGoMVC.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<Category> Categories => _appDbContext.Categories;
+
+        
+    }
+}
---
+++ DrinkRepository.cs
@@ -0,0 +1,27 @@
+﻿using DrinkAndGoMVC.Data;
+using DrinkAndGoMVC.Data.Interfaces;
+using DrinkAndGoMVC.Data.Models;
+using System.Data.Entity;
+
+namespace DrinkAndGoMVC.Data.Repositories
+{
+    public class DrinkRepository : IDrinkRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public DrinkRepository(AppDbContext appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+        }
+        
+        public IEnumerable<Drink> Drinks => _appDbContext.Drinks.Include(c => c.Category);
+        public IEnumerable<Drink> PreferredDrinks => _appDbContext.Drinks.Where(p => p.IsPreferredDrink).Include(c => c.Category);
+
+      
+        public Drink GetDrinksById(int drinkId) => _appDbContext.Drinks.FirstOrDefault(p => p.DrinkId == drinkId);
+        
+    }
+}
+
+    
+
